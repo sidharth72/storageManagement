21f379da4c1a1ef8915a24f9b6196c1466e31787
sidharth72 <74226199+sidharth72@users.noreply.github.com>
2020-11-22 11:05:25 +0530

    update move.py

--- move.py
+++ move.py
@@ -2,9 +2,12 @@ import os , shutil
 
 try:
     os.chdir('/storage/emulated/0/')
+    #path of file that want to be moved
     path = '/storage/emulated/0/DCIM/Camera'
-
+    
+    #looping through path we get all files in the folder
     for file in os.listdir(path):
+        #moving it by shutil module
         shutil.move(os.path.join(path,file),'/storage/emulated/0/Pictures/camera')
         print('successfully moved!')
 except:
